78361e65be38b3b04ad89e4c64939bdad853b892
Mateusz Czapliński <mateusz.c@kloud9.nyc>
2019-12-30 17:01:37 +0100

    disable old one

--- pkg/controller/gitopsconfig/controller.go
+++ pkg/controller/gitopsconfig/controller.go
@@ -237,7 +237,7 @@ func (r *Reconciler) createJob(jobtype string, instance *gitopsv1alpha1.GitOpsCo
 		log.Error(err, "unable to create the job", "job", job)
 		return err
 	}
-	go watchJobForStatus(r.client, job.Name, job.Namespace, instance.Name, instance.Namespace, time.Time{})
+	// go watchJobForStatus(r.client, job.Name, job.Namespace, instance.Name, instance.Namespace, time.Time{})
 	return nil
 }
 
@@ -291,7 +291,7 @@ func (r *Reconciler) createCronJob(instance *gitopsv1alpha1.GitOpsConfig) error 
 		log.Error(err, "Error in GetCronJob")
 		return err
 	}
-	go scheduleStatusForCronJobs(jobmonitor{r.client, cronjob.Name, cronjob.Namespace, time.Time{}}, result.Spec.Schedule, instance.Name, instance.Namespace)
+	// go scheduleStatusForCronJobs(jobmonitor{r.client, cronjob.Name, cronjob.Namespace, time.Time{}}, result.Spec.Schedule, instance.Name, instance.Namespace)
 	return nil
 }
 
