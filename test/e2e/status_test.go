9881a7103308fbb174d7f5dc37975abc312b0a04
Mateusz Czapliński <mateusz.c@kloud9.nyc>
2020-01-08 18:12:02 +0100

    WIP test periodic job success

--- test/e2e/status_test.go
+++ test/e2e/status_test.go
@@ -17,9 +17,6 @@ import (
 )
 
 func TestStatus_Succeeded(t *testing.T) {
-	fmt.Println("START JobFailed")
-	defer fmt.Println("END JobFailed")
-
 	ctx := framework.NewTestCtx(t)
 	defer ctx.Cleanup()
 
@@ -120,99 +117,110 @@ func TestStatus_Succeeded(t *testing.T) {
 	}
 }
 
-// // TestJobEvents_PeriodicJobSuccess verifies that a JobSuccessful event is
-// // emitted by eunomia for a Periodic GitOpsConfig.
-// func TestJobEvents_PeriodicJobSuccess(t *testing.T) {
-// 	ctx := framework.NewTestCtx(t)
-// 	defer ctx.Cleanup()
+func TestStatus_PeriodicJobSuccess(t *testing.T) {
+	ctx := framework.NewTestCtx(t)
+	defer ctx.Cleanup()
 
-// 	namespace, err := ctx.GetNamespace()
-// 	if err != nil {
-// 		t.Fatalf("could not get namespace: %v", err)
-// 	}
-// 	defer DumpJobsLogsOnError(t, framework.Global, namespace)
-// 	err = framework.AddToFrameworkScheme(apis.AddToScheme, &gitopsv1alpha1.GitOpsConfigList{})
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
+	namespace, err := ctx.GetNamespace()
+	if err != nil {
+		t.Fatalf("could not get namespace: %v", err)
+	}
+	defer DumpJobsLogsOnError(t, framework.Global, namespace)
+	err = framework.AddToFrameworkScheme(apis.AddToScheme, &gitopsv1alpha1.GitOpsConfigList{})
+	if err != nil {
+		t.Fatal(err)
+	}
 
-// 	eunomiaURI, found := os.LookupEnv("EUNOMIA_URI")
-// 	if !found {
-// 		eunomiaURI = "https://github.com/kohlstechnology/eunomia"
-// 	}
-// 	eunomiaRef, found := os.LookupEnv("EUNOMIA_REF")
-// 	if !found {
-// 		eunomiaRef = "master"
-// 	}
+	eunomiaURI, found := os.LookupEnv("EUNOMIA_URI")
+	if !found {
+		eunomiaURI = "https://github.com/kohlstechnology/eunomia"
+	}
+	eunomiaRef, found := os.LookupEnv("EUNOMIA_REF")
+	if !found {
+		eunomiaRef = "master"
+	}
 
-// 	// Step 1: register an event monitor/watcher
+	// Step 1: create a simple CR with a single Pod
 
-// 	events := make(chan *eventv1beta1.Event, 5)
-// 	closer, err := test.WatchEvents(framework.Global.KubeClient, events, namespace, "gitops-events-periodic-success", 180*time.Second)
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-// 	defer closer()
+	gitops := &gitopsv1alpha1.GitOpsConfig{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "GitOpsConfig",
+			APIVersion: "eunomia.kohls.io/v1alpha1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "gitops-status-periodic-success",
+			Namespace: namespace,
+		},
+		Spec: gitopsv1alpha1.GitOpsConfigSpec{
+			TemplateSource: gitopsv1alpha1.GitConfig{
+				URI:        eunomiaURI,
+				Ref:        eunomiaRef,
+				ContextDir: "test/e2e/testdata/status/test-b",
+			},
+			ParameterSource: gitopsv1alpha1.GitConfig{
+				URI:        eunomiaURI,
+				Ref:        eunomiaRef,
+				ContextDir: "test/e2e/testdata/empty-yaml",
+			},
+			Triggers: []gitopsv1alpha1.GitOpsTrigger{
+				{
+					Type: "Periodic",
+					Cron: "*/1 * * * *",
+				},
+			},
+			TemplateProcessorImage: "quay.io/kohlstechnology/eunomia-base:dev",
+			ResourceHandlingMode:   "Apply",
+			ResourceDeletionMode:   "Delete",
+			ServiceAccountRef:      "eunomia-operator",
+		},
+	}
+	gitops.Annotations = map[string]string{"gitopsconfig.eunomia.kohls.io/initialized": "true"}
 
-// 	// Step 2: create a simple CR with a single Pod
+	err = framework.Global.Client.Create(context.TODO(), gitops, &framework.CleanupOptions{TestContext: ctx, Timeout: timeout, RetryInterval: retryInterval})
+	if err != nil {
+		t.Fatal(err)
+	}
 
-// 	gitops := &gitopsv1alpha1.GitOpsConfig{
-// 		TypeMeta: metav1.TypeMeta{
-// 			Kind:       "GitOpsConfig",
-// 			APIVersion: "eunomia.kohls.io/v1alpha1",
-// 		},
-// 		ObjectMeta: metav1.ObjectMeta{
-// 			Name:      "gitops-events-periodic-success",
-// 			Namespace: namespace,
-// 		},
-// 		Spec: gitopsv1alpha1.GitOpsConfigSpec{
-// 			TemplateSource: gitopsv1alpha1.GitConfig{
-// 				URI:        eunomiaURI,
-// 				Ref:        eunomiaRef,
-// 				ContextDir: "test/e2e/testdata/events/test-b",
-// 			},
-// 			ParameterSource: gitopsv1alpha1.GitConfig{
-// 				URI:        eunomiaURI,
-// 				Ref:        eunomiaRef,
-// 				ContextDir: "test/e2e/testdata/empty-yaml",
-// 			},
-// 			Triggers: []gitopsv1alpha1.GitOpsTrigger{
-// 				{
-// 					Type: "Periodic",
-// 					Cron: "*/1 * * * *",
-// 				},
-// 			},
-// 			TemplateProcessorImage: "quay.io/kohlstechnology/eunomia-base:dev",
-// 			ResourceHandlingMode:   "Apply",
-// 			ResourceDeletionMode:   "Delete",
-// 			ServiceAccountRef:      "eunomia-operator",
-// 		},
-// 	}
-// 	gitops.Annotations = map[string]string{"gitopsconfig.eunomia.kohls.io/initialized": "true"}
+	// Step 2: watch Status till Succeeded & verify Status fields
 
-// 	err = framework.Global.Client.Create(context.TODO(), gitops, &framework.CleanupOptions{TestContext: ctx, Timeout: timeout, RetryInterval: retryInterval})
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
+	err = wait.Poll(retryInterval, 25*time.Second, func() (done bool, err error) {
+		fresh := gitopsv1alpha1.GitOpsConfig{}
+		err = framework.Global.Client.Get(context.TODO(), types.NamespacedName{Namespace: namespace, Name: gitops.Name}, &fresh)
+		if err != nil {
+			return false, err
+		}
+		switch fresh.Status.State {
+		case "InProgress":
+			// TODO: check that fresh.Status.StartTime is in the past, but after whole test started
+			if fresh.Status.CompletionTime != nil {
+				t.Errorf("want CompletionTime==nil, got: %#v", fresh.Status)
+			}
+			return false, nil
+		case "Succeeded":
+			if fresh.Status.CompletionTime == nil {
+				t.Errorf("CompletionTime==nil in: %#v", fresh.Status)
+			}
+			return true, nil
+		default:
+			t.Errorf("Unexpected State: %#v", fresh.Status)
+			return false, nil
+		}
+	})
+	if err != nil {
+		t.Error(err)
+	}
 
-// 	err = WaitForPodWithImage(t, framework.Global, namespace, "hello-events-test-b", "hello-app:1.0", retryInterval, 2*time.Minute)
-// 	if err != nil {
-// 		t.Error(err)
-// 	}
+	// Step 3: verify that the pod exists
 
-// 	// Step 3: Verify events
+	pod, err := GetPod(namespace, "hello-status-test-b", "hello-app:1.0", framework.Global.KubeClient)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if pod == nil || pod.Status.Phase != "Running" {
+		t.Fatalf("unexpected state of Pod: %v", pod)
+	}
 
-// 	// Verify there was an event emitted mentioning that Job finished successfully
-// 	select {
-// 	case event := <-events:
-// 		if event.Reason != "JobSuccessful" ||
-// 			event.DeprecatedSource.Component != "gitopsconfig-controller" {
-// 			t.Errorf("got bad event: %v", event)
-// 		}
-// 	case <-time.After(10 * time.Second):
-// 		t.Errorf("timeout waiting for JobSuccessful event")
-// 	}
-// }
+}
 
 func TestStatus_Failed(t *testing.T) {
 	fmt.Println("START Failed")
